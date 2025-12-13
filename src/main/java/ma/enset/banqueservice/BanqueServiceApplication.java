ce28d1f0932addc0b89940fbe7c2fa519f8e332f
mohamed-ameziane <med.ameez02@gmail.com>
2025-12-13 19:18:38 +0100

    Project Init

---
+++ src/main/java/ma/enset/banqueservice/BanqueServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.banqueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanqueServiceApplication.class, args);
+    }
+
+}
